6e2111c9af12fa0d94072460e00910c98aa95bfb
Helin <helin@bitbucket.org>
2022-12-09 23:45:12 +0300

    url validation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Button from 'react-bootstrap/Button';
-
+import UrlComponent from './components/UrlComponent';
 
 function App() {
   return (
-          <div className="text-center">
-               <div className="mb-3 mt-5">
-               <h1><strong>Silverlight</strong></h1>
-                <InputGroup>
-                  <Form.Control
-                    className="me-5 ms-5 mt-1"
-                    aria-label="Default"
-                    aria-describedby="inputGroup-sizing-default"
-                    placeholder="URL want to be checked"
-                    size="lg"
-                  />
-                </InputGroup>
-               </div>
-                <div className="d-grid mb-3 me-5 ms-5">
-                   <Button variant="primary" size="lg">
-                      Analyse
-                   </Button>
-                </div>
-          </div>
+    <div className="App">
+        <UrlComponent />
+    </div>
   );
 }
 
