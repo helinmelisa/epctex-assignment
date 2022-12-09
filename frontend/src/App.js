99b7d5581705949407440518fa5ba9f653872ff4
Helin <helin@bitbucket.org>
2022-12-09 23:02:39 +0300

    first page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="text-center">
+               <div className="mb-3 mt-5">
+               <h1><strong>Silverlight</strong></h1>
+                <InputGroup>
+                  <Form.Control
+                    className="me-5 ms-5 mt-1"
+                    aria-label="Default"
+                    aria-describedby="inputGroup-sizing-default"
+                    placeholder="URL want to be checked"
+                    size="lg"
+                  />
+                </InputGroup>
+               </div>
+                <div className="d-grid mb-3 me-5 ms-5">
+                   <Button variant="primary" size="lg">
+                      Analyse
+                   </Button>
+                </div>
+          </div>
   );
 }
 
