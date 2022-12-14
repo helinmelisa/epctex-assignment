ee93ed929ab80737f43d4bef752dfede6a899181
Helin Ergezen <70849887+helinmelisa@users.noreply.github.com>
2022-12-14 08:29:13 +0300

    add detail controller and wappalyzer

--- backend/controller/analysis.js
+++ backend/controller/analysis.js
@@ -1,17 +1,23 @@
 import Analysis from "../models/analysis.js";
-const Wappalyzer = require('wappalyzer');
-const { v4: uuidv4 } = require('uuid');
+import { v4 as uuidv4 } from 'uuid';
+import Wappalyzer from 'wappalyzer';
 
-export const getAnalysis = async (req,res) => {
+export const getAnalysisDetail = async (req,res) => {
     try{
-        // const analysis = await Analysis.findOne({ id: req.query.id });
-        // res.status(200).json(analysis)
         if (req.query.id) {
-            const analysis = await Analysis.findOne({ id: req.query.id });
+            const analysis = await Analysis.findById({ id: req.query.id });
             if (!analysis) {
-              res.status(400).json({ message: 'Id not found' });
-            } 
-        }
+              res.status(400).json({ message: 'cannot find' });
+            } else {
+                res.status(200).json({
+                  id: analysis.id,
+                  url: analysis.url,
+                  technologies: analysis.technologies,
+                  pageCount: analysis.pageCount,
+                  status: analysis.status,
+                });
+              }
+        } 
    } 
     catch(error){
         res.status(404).json({
@@ -20,21 +26,81 @@ export const getAnalysis = async (req,res) => {
     }
 };
 
+export const getAnalysis = async (req, res) => {
+    try {
+      const analysis = await Analysis.find();
+      res.status(200).json(analysis);
+    } catch (error) {
+      res.status(404).json({
+        message: error.message,
+      });
+    }
+  };
+
 export const createAnalysis = async (req, res) => {
     const analysis = req.body;
     const id = uuidv4();
-    const newAnalysis = new Analysis({
-        id: id,
-        name: analysis.name,
-        technologies: [],
-        status: 'PENDING',
-          }); 
     try{
-      await newAnalysis.save();
-    }
+        const analysis = req.body;
+        const newAnalysis = new Analysis({
+           id: Math.floor(Math.random() * 10000),
+           url: analysis.url,
+           technologies: [],
+           status: 'LOADING',
+             }); 
+         const savedAnalysis = await newAnalysis.save();
+         detectTech(id, analysis.url);
+         res.status(201).json(savedAnalysis);
+       }
     catch(error){
         res.status(409).json({
             message: error.message,
         });
     }
 };
+
+const options = {
+    debug: false,
+    delay: 500,
+    headers: {},
+    maxDepth: 4,
+    maxUrls: 10,
+    maxWait: 10000,
+    recursive: true,
+    probe: true,
+    proxy: false,
+    userAgent: 'Wappalyzer',
+    htmlMaxCols: 2000,
+    htmlMaxRows: 2000,
+    noScripts: false,
+    noRedirect: false,
+};
+  
+
+const detectTech = async (url) => {
+    const wappalyzer = new Wappalyzer(options);
+    try {
+        await wappalyzer.init();
+        const headers = {};
+        const site = await wappalyzer.open(url,headers);
+        site.on("error", console.error)
+        
+        const results = await site.analyze();
+        const pageCount = Object.keys(results.urls).length;
+        const techArray = [];
+            results.technologies.forEach((el) => {
+            techArray.push(el.url);
+    });
+
+        const result = await Analysis.findOne({ id: id });
+        result.technologies = techArray;
+        result.status = 'DONE';
+        result.pageCount = pageCount;
+        result.save();
+
+    } catch (error) {
+        await wappalyzer.destroy();
+        console.error(error);
+    }
+};
+
--- backend/routes/analysis.js
+++ backend/routes/analysis.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAnalysis, createAnalysis } from "../controller/analysis.js"
+import { getAnalysis, createAnalysis, getAnalysisDetail } from "../controller/analysis.js"
 
 const router = express.Router();
 
 router.get("/", getAnalysis);
+router.get("/:id", getAnalysisDetail);
 router.post("/", createAnalysis);
 
 export default router;
